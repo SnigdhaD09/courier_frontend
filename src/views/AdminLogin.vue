7343948e91a1cc64e96e0a855e25cc75aeac1d12
Sri Manoj Mopidevi <127987550+srimanojmopidevi@users.noreply.github.com>
2023-06-27 06:07:50 +0000

    cashier login screen is added

--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -5,21 +5,15 @@ import { useRouter } from "vue-router";
 import UserServices from "../services/UserServices.js";
 
 const router = useRouter();
-const isCreateAccount = ref(false);
 const snackbar = ref({
   value: false,
   color: "",
   text: "",
 });
 const user = ref({
-  firstName: undefined,
-  lastName: undefined,
   email: undefined,
   password: undefined,
-  address: undefined,
-  phoneNumber: undefined,
 });
-var homePageName;
 
 onMounted(async () => {
   if (localStorage.getItem("user") !== null) {
@@ -31,12 +25,22 @@ onMounted(async () => {
   }
 });
 
-function adminLogin() {
-  router.push({ name: 'adminlogin' });
-}
-
-function closeCreateAccount() {
-  isCreateAccount.value = false;
+async function login() {
+  console.log(user.value);
+  await UserServices.loginUser(user)
+    .then((data) => {
+      window.localStorage.setItem("user", JSON.stringify(data.data));
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = "Login successful!";
+      router.push({ name: "adminhomepage" });
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
 }
 
 function closeSnackBar() {
@@ -48,64 +52,24 @@ function closeSnackBar() {
   <v-container>
     <div id="body">
       <v-card class="rounded-lg elevation-5">
-        <v-card-title class="headline mb-2">Select User</v-card-title>
+        <v-card-title class="headline mb-2">Admin Login </v-card-title>
         <v-card-text>
-          <v-btn variant="flat" color="primary" @click="adminLogin()">Admin</v-btn>
+          <v-text-field
+            v-model="user.email"
+            label="Email"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="user.password"
+            label="Password"
+            required
+          ></v-text-field>
         </v-card-text>
+        <v-card-actions>
+          <v-btn variant="flat" color="primary" @click="login()">Login</v-btn>
+        </v-card-actions>
       </v-card>
-
-      <v-dialog persistent v-model="isCreateAccount" width="800">
-        <v-card class="rounded-lg elevation-5">
-          <v-card-title class="headline mb-2">Create Account </v-card-title>
-          <v-card-text>
-            <v-text-field
-              v-model="user.firstName"
-              label="First Name"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="user.lastName"
-              label="Last Name"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="user.address"
-              label="Address"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="user.phoneNumber"
-              label="Phone Number"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="user.email"
-              label="Email"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="user.password"
-              label="Password"
-              required
-            ></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              variant="flat"
-              color="secondary"
-              @click="closeCreateAccount()"
-              >Close</v-btn
-            >
-            <v-btn variant="flat" color="primary" @click="createAccount()"
-              >Create Account</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
 
       <v-snackbar v-model="snackbar.value" rounded="pill">
         {{ snackbar.text }}
